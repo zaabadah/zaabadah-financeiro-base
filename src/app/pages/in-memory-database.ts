aaef652b431d1004b9a613d727df77f3c11a25b8
zaabadah <zaabadah.moraes@gmail.com>
2021-11-12 10:01:19 -0300

    adicionando in-memory-database para categorias

---
+++ src/app/pages/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categorias = [
+            { id: 1, nome: 'Moradia', descricao: 'Pagamentos de contas da casa' }
+            { id: 2, nome: 'Saúde', descricao: 'Plano de Saúde e Remédios' }
+            { id: 3, nome: 'Lazer', descricao: 'Cinema, parques, praia, etc' }
+            { id: 4, nome: 'Salário', descricao: 'Recebimento de salário' }
+            { id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer' }
+        ];
+        return{categorias}
+    }
+}
