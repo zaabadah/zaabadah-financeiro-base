74e4ed7ada8f21e242403bd0cb247c33c7e032ba
zaabadah <zaabadah.moraes@gmail.com>
2021-11-12 10:37:38 -0300

    adicionando o categorias model

---
+++ src/app/pages/categorias/shared/categorias.model.ts
@@ -0,0 +1,7 @@
+export class Categorias {
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public descricao?: string
+    ){}
+}
--- src/app/pages/in-memory-database.ts
+++ src/app/pages/in-memory-database.ts
@@ -1,12 +1,13 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Categorias } from "src/app/pages/categorias/shared/categorias.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
         const categorias = [
-            { id: 1, nome: 'Moradia', descricao: 'Pagamentos de contas da casa' }
-            { id: 2, nome: 'Saúde', descricao: 'Plano de Saúde e Remédios' }
-            { id: 3, nome: 'Lazer', descricao: 'Cinema, parques, praia, etc' }
-            { id: 4, nome: 'Salário', descricao: 'Recebimento de salário' }
+            { id: 1, nome: 'Moradia', descricao: 'Pagamentos de contas da casa' },
+            { id: 2, nome: 'Saúde', descricao: 'Plano de Saúde e Remédios' },
+            { id: 3, nome: 'Lazer', descricao: 'Cinema, parques, praia, etc' },
+            { id: 4, nome: 'Salário', descricao: 'Recebimento de salário' },
             { id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer' }
         ];
         return{categorias}
