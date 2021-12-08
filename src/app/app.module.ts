1ec5df9b7698377ce0f5c867eb108dfa0babe919
zaabadah <zaabadah.moraes@gmail.com>
2021-12-08 10:22:24 -0300

    interceptando requisições com angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";//banco de dados virtual, retirar esta linha quando for real
+import { InMemoryDatabase } from "src/app/pages/in-memory-database";//banco de dados virtual, retirar esta linha quando for real
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)//banco de dados virtual, retirar esta linha quando for real
   ],
   providers: [],
   bootstrap: [AppComponent]
